a6b1df6f04617ffa6e8cb649e6896ad7b296a196
RidwanAhmed6 <155438343+RidwanAhmed6@users.noreply.github.com>
2025-01-30 13:07:35 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open("chat-admin").then((cache) => {
+            return cache.addAll([
+                "admin.html",
+                "manifest.json",
+                "icon-192x192.png",
+                "icon-512x512.png"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
 
